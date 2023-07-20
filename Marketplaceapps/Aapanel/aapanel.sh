d2d23758e656ea198cb0bb3207af135e24aace23
vigneshvrm <vigneshvrm@gmail.com>
2023-07-20 18:09:33 +0530

    updated the code

--- Marketplaceapps/Aapanel/aapanel.sh
+++ Marketplaceapps/Aapanel/aapanel.sh
@@ -6,5 +6,5 @@ apt install wget -y
 
 cd /usr/local/src/Aapanel && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/Aapanel/Aapanel-cleanup.sh
 
-cd /usr/local/src/Aapanel && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/Aapanel/aapanel.yaml
+cd /usr/local/src/Aapanel && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/Aapanel/aapanel.yml
 
