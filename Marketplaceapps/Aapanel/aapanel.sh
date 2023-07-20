0fe772c6307711972d50fd021ce540a7692311a8
vigneshvrm <vigneshvrm@gmail.com>
2023-07-20 18:13:09 +0530

    changed the code

--- Marketplaceapps/Aapanel/aapanel.sh
+++ Marketplaceapps/Aapanel/aapanel.sh
@@ -2,8 +2,6 @@
 
 mkdir -p /usr/local/src/Aapanel
 
-apt install wget -y
-
 cd /usr/local/src/Aapanel && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/Aapanel/Aapanel-cleanup.sh
 
 cd /usr/local/src/Aapanel && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/Aapanel/aapanel.yml
