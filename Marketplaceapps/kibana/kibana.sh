cc6c40ec76c4acb46410d7539aad89ae9cf7a075
vigneshvrm <vigneshvrm@gmail.com>
2023-07-31 12:08:59 +0530

    Added logstash

--- Marketplaceapps/kibana/kibana.sh
+++ Marketplaceapps/kibana/kibana.sh
@@ -5,3 +5,5 @@ mkdir -p /usr/local/src/kibana
 cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/kibana/kibana-cleanup.sh
 
 cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/kibana/kibana.yml
+
+cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/kibana/30-elasticsearch-output.conf
