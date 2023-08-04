d2979f71785f923d5c1e8bddfaf529e97fc11ad6
vigneshvrm <vigneshvrm@gmail.com>
2023-08-04 15:39:59 +0530

    code updated

--- Marketplaceapps/kibana/kibana.sh
+++ Marketplaceapps/kibana/kibana.sh
@@ -7,3 +7,5 @@ cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/
 cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/kibana/kibana.yml
 
 cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/kibana/30-elasticsearch-output.conf
+
+cd /usr/local/src/kibana && wget https://github.com/vigneshvrm/Workout/raw/main/Marketplaceapps/kibana/Kibana
