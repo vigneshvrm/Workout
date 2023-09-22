3f17321442637c1c1931619b0de5acc11514482a
vigneshvrm <vigneshvrm@gmail.com>
2023-09-22 16:20:21 +0530

    added y in jistmeet

--- Marketplaceapps/JitsiMeet/JitsiMeet-cleanup.sh
+++ Marketplaceapps/JitsiMeet/JitsiMeet-cleanup.sh
@@ -40,7 +40,7 @@ echo "127.0.1.1 $dom" | sudo tee -a /etc/hosts
 echo "jitsi-videobridge jitsi-videobridge/jvb-hostname $dom" | debconf-set-selections
 echo "jitsi-meet-web-config jitsi-meet/cert-choice select 'Generate a new self-signed certificate (You will later get a chance to obtain a Let's encrypt certificate)'" | debconf-set-selections
 
-apt install jitsi-meet
+apt install jitsi-meet -y
 
 certbot --nginx --non-interactive --redirect  -d $dom
 
