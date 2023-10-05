f8a6579d3e42576fdd9576a976a2e46c56d733fe
vigneshvrm <vigneshvrm@gmail.com>
2023-10-05 15:35:59 +0530

    yaml

--- Marketplaceapps/odoo/odoo.sh
+++ Marketplaceapps/odoo/odoo.sh
@@ -4,6 +4,6 @@ mkdir -p /usr/local/src/odoo
 
 cd /usr/local/src/odoo && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/odoo/odoo-cleanup.sh
 
-cd /usr/local/src/odoo && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/odoo/odoo.yml
+cd /usr/local/src/odoo && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/odoo/odoo.yaml
 
 cd /usr/local/src/odoo && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/odoo/odoo.conf
