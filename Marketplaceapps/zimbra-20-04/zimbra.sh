e8b4617eec179427f8768ff70a2d3ecb5f882f04
vigneshvrm <vigneshvrm@gmail.com>
2023-10-07 11:51:05 +0530

    cc

--- Marketplaceapps/zimbra-20-04/zimbra.sh
+++ Marketplaceapps/zimbra-20-04/zimbra.sh
@@ -4,5 +4,5 @@ mkdir -p /usr/local/src/zimbra
 
 cd /usr/local/src/zimbra && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/zimbra-20-04/zimbra-cleanup.sh
 
-cd /usr/local/src/zimbra && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/zimbra-20-04/zimbra.yml
+cd /usr/local/src/zimbra && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/zimbra-20-04/zimbra.yaml
 
