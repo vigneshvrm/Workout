2e2aa378525526da0471435eef125c065259b5c1
vigneshvrm <vigneshvrm@gmail.com>
2023-10-07 13:08:11 +0530

    modified

--- Marketplaceapps/zimbra-20-04/zimbra-cleanup.sh
+++ Marketplaceapps/zimbra-20-04/zimbra-cleanup.sh
@@ -9,6 +9,7 @@ echo -e "${RED}
 #                                 Passwords are stored under /root/                                            #
 ################################################################################################################
 ${NC}"
+
 pass=$(pwgen -ys 12 1)
 echo $pass > /root/.Zimbraadmin
 echo
@@ -16,10 +17,6 @@ echo -e "${RED}This message will be removed in the next login!${NC}"
 echo
 echo
 echo -e "${RED}The Zimbra Password is $(cat /root/.Zimbraadmin)"
-echo
-echo
-
-
 #To replace the Domain Name in the Zimbra configuration 
 a=0
 while [ $a -eq 0 ]
@@ -39,30 +36,24 @@ done
 # Prompt the user for the server timezone
 read -p "Enter the server timezone (e.g., 'America/New_York'): " user_timezone
 echo
-echo
 echo -e "${RED}Kindly wait till the Zimbra installation is completed${NC}"
-echo
-echo
 /usr/local/src/zimbra/zinstaller -p $pass -n mail -t $user_timezone $dom 
 echo
-echo
 echo -e "${RED}The zimbra installation is completed${NC}"
-echo
-echo
 
-# #Cleanup script
-# rm -rf /usr/local/src/
-# mkdir -p /usr/local/src/
-# rm -rf /var/lib/cloud/instances/*
-# rm -rf /var/lib/cloud/data/*
-# find /var/log -mtime -1 -type f -exec truncate -s 0 {} \; >/dev/null 2>&1
-# rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
-# cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
-# apt-get -y autoremove >/dev/null 2>&1
-# apt-get -y autoclean >/dev/null 2>&1
-# cat /dev/null > /root/.bash_history
-# unset HISTFILE
-# rm -rf /root/.bashrc
-# cp /etc/skel/.bashrc /root
-# rm -rf /opt/zimbra
-# history -c
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \; >/dev/null 2>&1
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+rm -rf /root/.bashrc
+cp /etc/skel/.bashrc /root
+rm -rf /opt/zimbra
+history -c
