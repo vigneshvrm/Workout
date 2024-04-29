f2db3cd3d6a1719df3ae11f0acd0004990921974
vigneshvrm <vigneshvrm@gmail.com>
2024-04-29 10:28:21 +0530

    updated necxtcloud.sh

--- Marketplaceapps/nextcloud/nextcloud.sh
+++ Marketplaceapps/nextcloud/nextcloud.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /usr/local/src/nextcloud
 
-cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/nextcloud/nextcloud.yaml
-cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/nextcloud/nextcloud.conf
-cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/nextcloud/nextcloud-cleanup.sh
+cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nextcloud/nextcloud.yaml
+cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nextcloud/nextcloud.conf
+cd /usr/local/src/nextcloud && wget https://raw.githubusercontent.com/stackbill/marketplace/main/nextcloud/nextcloud-cleanup.sh
