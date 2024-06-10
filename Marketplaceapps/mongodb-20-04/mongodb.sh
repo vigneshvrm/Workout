e1db1929d8b894cf2a036e32143d59d02a2a32a9
vigneshvrm <vigneshvrm@gmail.com>
2024-06-10 16:27:23 +0530

    git added and mongo removed

--- Marketplaceapps/mongodb-20-04/mongodb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p {/usr/local/src/mongodb-20-04/opt/cloudstack/,/usr/local/src/mongodb-20-04/}
-
-cd /usr/local/src/mongodb-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/cleanup.sh
-
-cd /usr/local/src/mongodb-20-04/ && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/mongodb-20-04/mongodb.yaml
