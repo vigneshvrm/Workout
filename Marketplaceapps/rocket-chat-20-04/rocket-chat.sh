b92c19e7b9194df919a3eb51677bd4b11c5f8d4c
vigneshvrm <vigneshvrm@gmail.com>
2024-06-18 16:55:41 +0530

    changed to podman

--- Marketplaceapps/rocket-chat-20-04/rocket-chat.sh
+++ Marketplaceapps/rocket-chat-20-04/rocket-chat.sh
@@ -4,4 +4,4 @@ mkdir -p {/usr/local/src/rocket-chat-20-04/opt/cloudstack/,/usr/local/src/rocket
 
 cd /usr/local/src/rocket-chat-20-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/rocket-chat-cleanup.sh
 
-cd /usr/local/src/rocket-chat-20-04/ && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/rocket-chat-20-04/rocket-chat.yaml
+cd /usr/local/src/rocket-chat-20-04/ && wgethttps://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/rocket-chat-20-04/rocket-chat.yaml
