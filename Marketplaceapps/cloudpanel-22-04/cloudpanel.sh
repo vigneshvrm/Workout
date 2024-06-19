24e8516eb8165dd46ede036e58de04c2b6e6aef9
vigneshvrm <vigneshvrm@gmail.com>
2024-06-19 10:47:13 +0530

    cloudpanel

--- Marketplaceapps/cloudpanel-22-04/cloudpanel.sh
+++ Marketplaceapps/cloudpanel-22-04/cloudpanel.sh
@@ -4,4 +4,4 @@ mkdir -p {/usr/local/src/cloudpanel-22-04/opt/cloudstack/,/usr/local/src/cloudpa
 
 cd /usr/local/src/cloudpanel-22-04/opt/cloudstack/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/_common-files/opt/cloudstack/cleanup.sh
 
-cd /usr/local/src/cloudpanel-22-04/ && wget https://raw.githubusercontent.com/stackbill/marketplace/main/cloudpanel-22-04/cloudpanel.yaml
+cd /usr/local/src/cloudpanel-22-04/ && wget https://raw.githubusercontent.com/vigneshvrm/Workout/main/Marketplaceapps/cloudpanel-22-04/cloudpanel.yaml
