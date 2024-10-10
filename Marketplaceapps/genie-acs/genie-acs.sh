81d8214f04ca7011bcd8f4aac3bf261d0f8ec1a4
vigneshvrm <vigneshvrm@gmail.com>
2024-10-10 16:11:52 +0530

    genie-acs-cleanup.sh

--- Marketplaceapps/genie-acs/genie-acs.sh
+++ Marketplaceapps/genie-acs/genie-acs.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /usr/local/src/genie-acs
 
-cd /usr/local/src/genie-acs && wget https://raw.githubusercontent.com/vigneshvrm/Workout/refs/heads/main/Marketplaceapps/genie-acs-cleanup.sh
+cd /usr/local/src/genie-acs && wget https://raw.githubusercontent.com/vigneshvrm/Workout/refs/heads/main/Marketplaceapps//genie-acs/genie-acs-cleanup.sh
 
 cd /usr/local/src/genie-acs && wget https://raw.githubusercontent.com/vigneshvrm/Workout/refs/heads/main/Marketplaceapps/genie-acs/genie-acs.yaml
